a7dc0bb31b9356e110493403b9eb56d0eaaece2a
die4glory <156427768+die4glory@users.noreply.github.com>
2024-03-14 19:48:01 +0300

    Update strongship.h

--- strongship.h
+++ strongship.h
@@ -10,6 +10,7 @@ class strongShip:public Spaceships {
 public:
     strongShip(): Spaceships(0.5){}
 
+    //UPDATES
     int updateHealthDamage10(int& _health) override {
         health -= damage*damageCoef;
         return health;
@@ -26,9 +27,10 @@ public:
         }
     }
 
+    //EVENTS
     void asteroid() override {
         int asteroidDamage = (rand() % 4) == 0&1&2 ? 10: 0;
-        std::cout<<"Asteroid kuşağından geçiyorsun!\n\n";
+        std::cout<<"\nAsteroid kuşağından geçiyorsun!\n\n";
         std::cout<<"                      .:'\n"
                    "         ....     _.::'\n"
                    "       .:-\"\"-:.  (_.'\n"
@@ -40,23 +42,28 @@ public:
         if(asteroidDamage>0) {
             updateHealthDamage10(health);
             std::cout<<"\nAsteroidlerden birine çarpıp 5 hasar aldın.\n\n";
+            displayStatus();
         }
         else {
-            std::cout<<"\nAsteroid kuşağından hasar almadan geçtin. Yakıtın azaldı.\n\n";
+            std::cout<<"\nAsteroid kuşağından hasar almadan geçtin. Yakıtın azaldı.\n";
             updateFuel(fuel);
+            displayStatus();
         }
     }
 
+    //ACTIONS
     void run() override {
         int oddsOfFlee =  (rand() % 4) == 0&1&2 ? 10: 0;
         if(oddsOfFlee>0) {
             updateFuel(fuel);// hocam kacamasa bile yakit harcanir seklinde yaptik
             std::cout<<"Yakıtın azaldı. Tam kaçabildiğini sandığın anda...\n\n";
             pirates();
+            displayStatus();
         }
         else {
             updateFuel(fuel);
             std::cout<<"Korsanlara izini kaybettirdin! Yakıtın azaldı.\n\n";
+            displayStatus();
         }
     }
 
@@ -68,6 +75,7 @@ public:
         else {
             std::cout<<"Savaşı kaybettin. 15 hasar aldın.\n\n";
             updateHealthDamage30(health);
+            displayStatus();
         }
     }
 
