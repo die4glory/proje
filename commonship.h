72d944c79b09d32072d7f1dd09428bb0bbd54fbe
die4glory <156427768+die4glory@users.noreply.github.com>
2024-03-14 20:03:08 +0300

    Update commonship.h

--- commonship.h
+++ commonship.h
@@ -1,5 +1,5 @@
 //
-// Created by Duru Irmak Öztürk on 21.02.2024.
+// Created by Duru Irmak Öztürk on 13.03.2024.
 //
 
 #ifndef UNTITLED_COMMONSHIP_H
@@ -8,85 +8,22 @@
 
 class commonShip:public Spaceships {
 public:
-    commonShip(): Spaceships(1){}
+    //CONSTRUCTOR
+    //commonShip():Spaceships(double _damageCoef):damageCoef{_damageCoef}{}
 
-    int updateHealthDamage10(int& _health) override {
-       if (health>=0) {
-           health -= damage*damageCoef;
-           return health;
-       }
-       else {
-           health= 0;
-           return health;
-       }
-    }
+    //UPDATES
+    int updateHealthDamage10(int& _health) override;
+    int updateHealthDamage30(int& _health) override;
 
-    int updateHealthDamage30(int& _health) override {
-        if (health>=0) {
-            health -= damage_*damageCoef;
-            return health;
-        }
-        else {
-            health= 0;
-            return health;
-        }
-    }
+    //EVENTS
+    void asteroid() override;
 
-    void asteroid() override {
-        int asteroidDamage = (rand() % 4) == 0&&1 ? 10 : 0;
-        std::cout<<"\nAsteroid kuşağından geçiyorsun!\n\n";
-        std::cout<<"                      .:'\n"
-                   "         ....     _.::'\n"
-                   "       .:-\"\"-:.  (_.'\n"
-                   "     .:/      \\:.\n"
-                   "     :|        |:\n"
-                   "     ':\\      /:'\n"
-                   "      '::-..-::'\n"
-                   "        `''''`";
-        if(asteroidDamage>0) {
-            updateHealthDamage10(health);
-            std::cout<<"\nAsteroidlerden birine çarpıp 10 hasar aldın.\n\n";
-            displayStatus();
-        }
-        else {
-            std::cout<<"\nAsteroid kuşağından hasar almadan geçtin.Yakıtın azaldı.\n";
-            updateFuel(fuel);
-            displayStatus();
-        }
-    }
-
-    void run() override {
-        updateFuel(fuel);
-        int oddsOfFlee = (rand() % 4) == 0 && 1 ? 10 : 0;
-        if (oddsOfFlee > 0) {
-            updateFuel(fuel);// hocam kacamasa bile yakit harcanir seklinde yaptik
-            std::cout<<"Yakıtın azaldı. Tam kaçabildiğini sandığın anda...\n\n";
-            pirates();
-            displayStatus();
-        }
-        else {
-            updateFuel(fuel);
-            std::cout<<"Korsanlara izini kaybettirdin! Yakıtın azaldı.\n\n";
-            displayStatus();
-        }
-    }
-
-    void fight() override {
-        int oddsOfWin = (rand() % 4) == 0&&1 ? 10 : 0;
-        if (oddsOfWin>0) {
-            std::cout<<"Savaşın galibi sensin!\n\n";
-
-        }
-        else {
-            std::cout<<"Savaşı kaybettin. 30 hasar aldın.\n\n";
-            updateHealthDamage30(health);
-            displayStatus();
-        }
-    }
+    //ACTIONS
+    void run() override;
+    void fight() override;
 
 private:
 protected:
 
 };
-
 #endif //UNTITLED_COMMONSHIP_H
