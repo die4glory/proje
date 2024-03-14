0ecc202ad8ef12d3ea0841e8062988e6600f94ad
die4glory <156427768+die4glory@users.noreply.github.com>
2024-03-14 20:02:29 +0300

    Update commonship.cpp

--- commonship.cpp
+++ commonship.cpp
@@ -2,6 +2,88 @@
 //
 // Created by Duru Irmak Öztürk on 13.03.2024.
 //
-#include "spaceships.h"
 #include "commonship.h"
 #include <iostream>
+
+class commonShip:public Spaceships {
+public:
+    commonShip(): Spaceships(1){}
+
+    int updateHealthDamage10(int& _health) override {
+       if (health>=0) {
+           health -= damage*damageCoef;
+           return health;
+       }
+       else {
+           health= 0;
+           return health;
+       }
+    }
+
+    int updateHealthDamage30(int& _health) override {
+        if (health>=0) {
+            health -= damage_*damageCoef;
+            return health;
+        }
+        else {
+            health= 0;
+            return health;
+        }
+    }
+
+    void asteroid() override {
+        int asteroidDamage = (rand() % 4) == 0&&1 ? 10 : 0;
+        std::cout<<"\nAsteroid kuşağından geçiyorsun!\n\n";
+        std::cout<<"                      .:'\n"
+                   "         ....     _.::'\n"
+                   "       .:-\"\"-:.  (_.'\n"
+                   "     .:/      \\:.\n"
+                   "     :|        |:\n"
+                   "     ':\\      /:'\n"
+                   "      '::-..-::'\n"
+                   "        `''''`";
+        if(asteroidDamage>0) {
+            updateHealthDamage10(health);
+            std::cout<<"\nAsteroidlerden birine çarpıp 10 hasar aldın.\n\n";
+            displayStatus();
+        }
+        else {
+            std::cout<<"\nAsteroid kuşağından hasar almadan geçtin.Yakıtın azaldı.\n";
+            updateFuel(fuel);
+            displayStatus();
+        }
+    }
+
+    void run() override {
+        updateFuel(fuel);
+        int oddsOfFlee = (rand() % 4) == 0 && 1 ? 10 : 0;
+        if (oddsOfFlee > 0) {
+            updateFuel(fuel);// hocam kacamasa bile yakit harcanir seklinde yaptik
+            std::cout<<"Yakıtın azaldı. Tam kaçabildiğini sandığın anda...\n\n";
+            pirates();
+            displayStatus();
+        }
+        else {
+            updateFuel(fuel);
+            std::cout<<"Korsanlara izini kaybettirdin! Yakıtın azaldı.\n\n";
+            displayStatus();
+        }
+    }
+
+    void fight() override {
+        int oddsOfWin = (rand() % 4) == 0&&1 ? 10 : 0;
+        if (oddsOfWin>0) {
+            std::cout<<"Savaşın galibi sensin!\n\n";
+
+        }
+        else {
+            std::cout<<"Savaşı kaybettin. 30 hasar aldın.\n\n";
+            updateHealthDamage30(health);
+            displayStatus();
+        }
+    }
+
+private:
+protected:
+
+};
