8115be9b8eb269e908785260933ac481b6651733
die4glory <156427768+die4glory@users.noreply.github.com>
2024-03-14 10:40:23 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "events.h"
+#include "spaceships.h"
+#include <cstdlib>
+
+int main() {
+
+    srand(time(0));
+    std::shared_ptr<Spaceships>secilen_gemi;
+    Events::start_game();
+
+
+    return 0;
+}
